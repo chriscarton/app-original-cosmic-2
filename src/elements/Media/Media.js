8d459153cb9fada7e939abf205991d14253ee0f7
Chris <chriscarton@live.fr>
2019-10-29 16:51:36 +0100

    autodeploy

--- src/elements/Media/Media.js
+++ src/elements/Media/Media.js
@@ -36,6 +36,7 @@ export class Media extends Component {
                         autoPlay 
                         loop
                         muted
+                        playsinline={false}
                     >
                         <source 
                             src={process.env.PUBLIC_URL+'/'+this.props.path+'/'+media.src} 

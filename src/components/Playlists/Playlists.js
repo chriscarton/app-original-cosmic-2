e9eee2698b4f4aa087e1fb3ed4f7146b29ac44ae
Chris <chriscarton@live.fr>
2019-11-07 16:46:21 +0100

    Bon la navigation projets type playlists c'est ok

--- src/components/Playlists/Playlists.js
+++ src/components/Playlists/Playlists.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Media from '../../elements/Media/Media.js';
 import './Playlists.scss';
+import { Link } from 'react-router-dom';
+import ProjectNav from '../../elements/ProjectNav/ProjectNav.js';
 
 export class Playlists extends Component {
 
@@ -14,6 +16,20 @@ export class Playlists extends Component {
 
         return (
             <div id="Playlists">
+                <div className="bar topbar">
+                    <Link to="/projets">
+                        <i className="fa fa-chevron-left"></i>
+                        &nbsp;Retour
+                    </Link>
+                    <div id="projectNav">
+                        {this.props.item.prev &&
+                            <ProjectNav direction="prev" arrow="left" link={`/projet/${this.props.item.prev.slug}`} text="Précédent" />
+                        }
+                        {this.props.item.next &&
+                            <ProjectNav direction="next" arrow="right" link={`/projet/${this.props.item.next.slug}`} text="Suivant" />
+                        }
+                    </div>
+                </div>
                 {props.item.medias.map((media,index)=>(
                     <div className="media">
                         <Media 
@@ -23,6 +39,13 @@ export class Playlists extends Component {
                         />
                     </div>
                 ))}
+                <div className="bar bottombar">
+                    <div className="to-contact">
+                        <Link to="/studio" onClick={this.scrollToForm}>
+                            <i className="fa fa-envelope" aria-hidden="true"></i> Contact
+                        </Link>
+                    </div>
+                </div>
             </div>
         )
     }
